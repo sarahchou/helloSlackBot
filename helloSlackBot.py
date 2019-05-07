1443a050012b39c07c488fbc11cb0b991c514b77
Sarah Chou <Sarah.Chou@vistaprint.com>
2019-05-07 09:46:59 -0400

    clean up of unnecessary code

--- helloSlackBot.py
+++ helloSlackBot.py
@@ -15,23 +15,22 @@ sched_minute = 40
 
 channel = 'CJ9S864SG'
 
-#for some reason, the hour is 4 hours ahead. To schedule at 9am, have to make it 13...
+# For some reason, the hour is 4 hours ahead. i.e. to schedule at 9am, have to make it 13...
 sched_time = (datetime.datetime(sched_year,sched_month,sched_day,sched_hour + 4,sched_minute) - datetime.datetime(1970,1,1)).total_seconds()
 
-postTime = sched_time
+# Put whatever message you want here:
 message = "Message scheduled for " + str(sched_hour) + ":" + str(sched_minute)
 
 headers.update({'Content-Type': 'application/json'})
-headers.update({'Authorization': 'Bearer XXXXXXXXXXX'})
+headers.update({'Authorization': 'Bearer XXXXXXXXX'})
 
 data.update({'channel': channel})
 data.update({'text': message})
-data.update({'post_at': postTime})
+data.update({'post_at': sched_time})
 
 data_json = json.dumps(data)
 
 r = requests.post(uri, headers=headers, data=data_json)
 
+# For testing purposes, print the json output of the request: 
 print(json.dumps(r.json(), sort_keys=True, indent=4))
-
-print(time.localtime)
