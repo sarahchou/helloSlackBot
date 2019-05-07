59b6460166f5a74b4737ca39fc7656799478ed9b
Sarah Chou <Sarah.Chou@vistaprint.com>
2019-05-07 09:48:54 -0400

    Adding documentation

--- helloSlackBot.py
+++ helloSlackBot.py
@@ -3,24 +3,26 @@ import json
 import time
 import datetime
 
+# Initializing Variables
 headers = {}
 data = {}
 uri = 'https://slack.com/api/chat.scheduleMessage'
+channel = 'CJ9S864SG'
 
+# Put in variables for the scheduled time to be posted at here: 
 sched_year = 2019
 sched_month = 5
 sched_day = 7
 sched_hour = 9
 sched_minute = 40
 
-channel = 'CJ9S864SG'
-
 # For some reason, the hour is 4 hours ahead. i.e. to schedule at 9am, have to make it 13...
 sched_time = (datetime.datetime(sched_year,sched_month,sched_day,sched_hour + 4,sched_minute) - datetime.datetime(1970,1,1)).total_seconds()
 
-# Put whatever message you want here:
+# Put message content you want here:
 message = "Message scheduled for " + str(sched_hour) + ":" + str(sched_minute)
 
+# Fill in headers and data dictionaries
 headers.update({'Content-Type': 'application/json'})
 headers.update({'Authorization': 'Bearer XXXXXXXXX'})
 
@@ -30,6 +32,7 @@ data.update({'post_at': sched_time})
 
 data_json = json.dumps(data)
 
+# Make the request
 r = requests.post(uri, headers=headers, data=data_json)
 
 # For testing purposes, print the json output of the request: 
